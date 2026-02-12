68b6ec3bfe07e04bcbdd2e415e881cb21652901a
rajpootankush222-ctrl <rajpootankush222@gmail.com>
2026-02-12 18:50:42 +0530

    Update generate-persona.js

--- pages/api/generate-persona.js
+++ pages/api/generate-persona.js
@@ -2,8 +2,7 @@
 // This is a Vercel serverless function
 
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+  console.log("BODY:", req.body);
   }
 
   const { answers, photo } = req.body;
